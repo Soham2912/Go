87f0400347fadc79761068012a1000c6116dcecc
Soham2912 <sohamlahoti@gmail.com>
2024-06-28 23:59:22 +0530

    Add print statements and initialize Person struct in main.go

--- struct/main.go
+++ struct/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Person struct {
 	Name    string
 	Surname string
@@ -8,5 +10,18 @@ type Person struct {
 
 func main() {
 	var void Person
-	
+	fmt.Println(void)
+	void.Name= "Void"
+	void.Surname= "wake"
+	void.Age= 25
+	fmt.Println(void)
+
+	person2 := Person{
+		Name: "bruce",
+		Surname: "wayne",
+		Age: 30,
+	}
+
+	fmt.Println(person2)
+
 }
