3127610c866d8b4d2b78c984f650441e442657d8
Soham2912 <sohamlahoti@gmail.com>
2024-06-28 22:16:19 +0530

    If-else in Go

---
+++ ifelse/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	if x > 5 {
+		fmt.Println("x > 5")
+	} else if x < 3  {
+		fmt.Println("x < 3")
+	} else {
+		fmt.Println("x",10)
+		
+	}
+}
