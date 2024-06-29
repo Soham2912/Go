19604dffde9b7bb9d7adb0daa9a95628f4f3a8c2
Soham2912 <sohamlahoti@gmail.com>
2024-06-29 10:15:13 +0530

    Refactor main.go for improved code readability and efficiency

--- DataConv/main.go
+++ DataConv/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 
 	var num int = 42
-	fmt.Printf("type %T ", num)
-	
+	fmt.Printf("type %T \n", num)
 
+	var data float32 = float32(num)
+	fmt.Printf("type %T \n", data)
+
+	num = 123
+	str := strconv.Itoa(num)
+	fmt.Printf("type %T \n", str)
+
+	str = "1234"
+	num, _ = strconv.Atoi(str)
+	fmt.Printf("type %T \n", num)
+
+	var num_float float64
+
+	str = "3.14"
+	num_float, _ = strconv.ParseFloat(str, 64)
+	fmt.Printf("type %T ", num_float)
 }
